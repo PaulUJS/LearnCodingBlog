40034a2689bf3a1019aaccf80c1689cffbd458e3
Paul Ulerick <p.ulerick17@gmail.com>
2022-10-24 18:44:01 -0400

    created db connection

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const express = require('express')
 const res = require('express/lib/response')
+const { send, sendStatus } = require('express/lib/response')
 const mysql = require('mysql2')
+const { error } = require("console")
+require('dotenv').config()
+
+// Creates the db connection
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+})
