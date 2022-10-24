2e5fff32b7b9d614f065d5353f237700f400d20c
Paul Ulerick <p.ulerick17@gmail.com>
2022-10-24 19:23:14 -0400

    finished basic file structure, wrote all the routes, and functions for the website

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const mysql = require('mysql2')
 const { error } = require("console")
 require('dotenv').config()
 
+// Gets form info from html forms and reads as json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// View engine for rendering dynamic html pages
+app.set('view engine', 'ejs')
+
 // Creates the db connection
 const db = mysql.createConnection({
     host: process.env.HOST,
@@ -13,13 +20,50 @@ const db = mysql.createConnection({
     database: process.env.DATABASE,
 })
 
+// Connects to the database once the file is run
 db.connect((err) => {
     if (err) throw err;
 })
 
+// Creates the express app
 const app = express()
 
+// Creates the server for the app to run on
 app.listen('3000', () => {
     if (err) throw err
     console.log('Server on')
+})
+
+//
+// Main routes and functions
+
+// Gets post at specified ID
+function getPost() {
+
+}
+
+// Creates post and pushes it to main page
+function createPost() {
+
+}
+
+// Deletes post at specified ID
+function deletePost() {
+
+}
+
+// Edits post at specified ID
+function editPost() {
+    
+}
+
+// Renders the landing page
+app.get('/', (req,res) => {
+    try {
+        res.render('index')
+    }
+    catch (err) {
+        console.log('error caught')
+        res.send('There was an error, try again or come back another time.')
+    }
 })
